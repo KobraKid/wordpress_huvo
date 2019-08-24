2be067f963fb9117be5d9c33d2a463f109dfca9d
Michael Huyler <michaelhuyler@gmail.com>
2019-08-24 00:38:51 -0500

    initial upload

---
+++ wp-content/themes/onepress-child/inc/customize-configs/section-upsell.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Section: Order & Styling - PLUS VERSION
+ */
+
+
+// Register custom section types.
+$wp_customize->register_section_type( 'OnePress_Section_Plus' );
+// Register sections.
+$wp_customize->add_section(
+	'onepress_order_styling',
+	array(
+		'title'           => esc_html__( 'Section Order & Styling', 'onepress' ),
+		'priority'        => 129,
+	)
+);
+
+// Plus message
+$wp_customize->add_setting( 'onepress_order_styling_message',
+	array(
+		'sanitize_callback' => 'onepress_sanitize_text',
+	)
+);
+$wp_customize->add_control( new OnePress_Misc_Control( $wp_customize, 'onepress_order_styling_message',
+	array(
+		'type'        => 'custom_message',
+		'section'     => 'onepress_order_styling',
+		'description' => __('<h4 class="customizer-group-heading-message">Drag &amp; Drop Section Orders</h4><p class="customizer-group-heading-message">Check out the <a target="_blank" href="https://www.famethemes.com/plugins/onepress-plus/?utm_source=theme_customizer&utm_medium=text_link&utm_campaign=onepress_customizer#get-started">OnePress Plus</a> version for full control over the frontpage SECTIONS ORDER!</p><h4 class="customizer-group-heading-message">Advanced Section Styling</h4><p class="customizer-group-heading-message">Check out the <a target="_blank" href="https://www.famethemes.com/plugins/onepress-plus/?utm_source=theme_customizer&utm_medium=text_link&utm_campaign=onepress_customizer#get-started">OnePress Plus</a> version for full control over the section styling which includes background color, image, video, parallax effect, custom style and more ...</p>', 'onepress' )
+	)
+));
